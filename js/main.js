5812c6d37336eff1044e936da4e1c2759c20ac6a
Jeremy Evans <jeremy.n.evans@gmail.com>
2016-09-06 18:25:38 +0100

    FAQs fixed

--- js/main.js
+++ js/main.js
@@ -1,10 +1,9 @@
 // JavaScript
 window.sr = ScrollReveal();
-// sr.reveal('.max-width');
-sr.reveal('.scroll-reveal, h4.panel-title', { distance: '40px', duration: 600, scale: 0.95 });
-
+sr.reveal('.scroll-reveal', { distance: '40px', duration: 600, scale: 0.95 });
+//
 $(function() {
-  $('a[href*="#"]:not([href="#"])').click(function() {
+  $('a[href*="#about"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
