44cf49fd04a08767d9d22e3352235e2659db5645
Jeremy Evans <jeremy.n.evans@gmail.com>
2016-09-06 18:10:47 +0100

    Scrolling effects

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// JavaScript
+window.sr = ScrollReveal();
+// sr.reveal('.max-width');
+sr.reveal('.scroll-reveal, h4.panel-title', { distance: '40px', duration: 600, scale: 0.95 });
+
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 600);
+        return false;
+      }
+    }
+  });
+});
